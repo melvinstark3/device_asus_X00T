2b16aa8d424545c2acfaea0a99ff293f7d618803
Chris <chris@cypheros.co>
2018-09-20 04:32:48 -0400

    Disables hardware Composition
    Change-Id: Ie7dda288e313b03fab7ff911e84f21a6f288b0ac
    Signed-off-by: Melvin <kartik3163@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -106,7 +106,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
-    debug.sf.latch_unsignaled=1 \
+    debug.sf.latch_unsignaled=0 \
     vendor.gralloc.enable_fb_ubwc=1 \
     debug.hwui.use_buffer_age=false \
     dev.pm.dyn_samplingrate=1 \
@@ -117,6 +117,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_default_color_mode=1
     vendor.display.disable_inline_rotator=1 \
     vendor.display.disable_partial_split=1 \
+    debug.sf.disable_hwcomposer=1
 
 # Enable QC2 bufferqueue block-pool
 PRODUCT_PROPERTY_OVERRIDES += vendor.qc2.use.bqpool=1
