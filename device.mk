01b5126eae625f72f08525c805ff49b908951ee0
John Dias <joaodias@google.com>
2020-01-09 23:49:31 +0530

    Pre-opt SystemUI Properly
    SystemUI is getting deoptimized, possibly resulting in jank
    (not yet validated); pre-optimize to avoid deoptimization for now.
    Bug: 62832717
    Bug: 35872915
    Test: boot, check for vdex, run for a bit
    
    Signed-off-by: Melvin <kartik3163@gmail.com>

--- device.mk
+++ device.mk
@@ -412,6 +412,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson
 
+# Pre-opt SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
